4a2cf2963735ac5792fbd015fd3a32af24f2c585
Freder-chen <freder-chen@qq.com>
2020-10-13 15:28:56 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# Python 生成课表(.ics)导入Mac日历
+
+![image-20201](https://tva1.sinaimg.cn/large/007S8ZIlly1gjnpdhndsfj31970u0n4s.jpg)
+
+## 简介
+
+学校研究生没有本科待遇，课表没有录入系统😢，遂借鉴**[junyilou/python-ical-timetable](https://github.com/junyilou/python-ical-timetable)**适配了课程。项目生成ics文件导入Apple设备的日历应用，可以方便随时查看课表。
+
+## 功能
+* 支持录入课程名称，教师，上课地点，上课时间（也可以通过修改代码自己添加附加信息）
+
+## 使用
+请调整`main`函数中的以下内容来适配课表：
+
+1. **maxWeek** 为本学期最大周数，可适当加大，但不可小于实际周数。
+
+2. **classTime** 为每节课的**上课时间**（下课时间逻辑在`timetable`函数中）。直接填写每节课的 24 小时制上课时间：例如 8:00 上课，则录入`(8, 0)`；下午 7:50 上课，则录入`(19, 50)`。
+
+3. 修改 **starterDay** 为本学期第一周星期一的日期。
+
+4. 修改 **classes** 中的课程信息，由于不同学校课表可能含有不同信息，请参考源代码中的课表填写，并直接在后续定义中作出相应修改。
+
+  **设置周数：**
+单独周：请改为数组形式，例如 [2]；
+  范围周：请使用`rgWeek`，例如 rgWeek(3, 7) 代表第三周到第七周；
+  奇数周：请使用`oeWeek`，例如 oeWeek(2, 9, 1) 代表第二周到第九周的单数周，将 1 改为 0 即为偶数周。
+  
+  **设置课程节数：**
+一节课：请改为数组形式，例如 [2]；
+  范围课，请使用`rgWeek`，例如 rgWeek(3, 7) 代表第三节一直上到第七节；
+5. 一节课的时间由`oneClassTime`表示，默认为45分钟，可自行修改。
+6. 更改`filename`将ics文件保存在合适的位置。
+
+### 导入方法
+
+### [Mac导入日历(推荐)](https://support.apple.com/zh-cn/guide/calendar/icl1023/mac)
+
+1. 在 Mac 上的“日历” App <img src="https://help.apple.com/assets/5E1912A2094622D13119F732/5E1912A5094622D13119F739/zh_CN/4f2c844f7eff8d8e1fdbd8ea562db18d.png" alt="img" style="zoom:25%;" /> 中，为日程[创建新日历](https://support.apple.com/zh-cn/guide/calendar/icl1005/11.0/mac/10.15)（如有需要）。
+2. 选取“文件”>“导入”。
+3. 选择带有日程的文件，然后点按“导入”。
+4. 选择日历来添加日程。
+
+导入Mac后，可通过共享供IOS设备使用。
+
+<img src="https://tva1.sinaimg.cn/large/007S8ZIlly1gjnqf5nhc8j30i00aoqa5.jpg" alt="image-20201" style="zoom: 50%;" />
+
+### [IOS导入日历](https://support.apple.com/zh-cn/guide/iphone/ipha0d932e96/ios)
+
+1. 前往“设置”<img src="https://help.apple.com/assets/5EB05D710946221361B50B6B/5EB05D7C0946221361B50BAA/zh_CN/492fec5aff74dbdef9b526177c3804b4.png" alt="img" style="zoom:25%;" /> >“日历">“帐户">“添加帐户”。
+2. 轻点“其他”，然后执行以下任一项操作：
+    - *订阅 iCal (.ics) 日历：*轻点“添加已订阅的日历”，然后输入订阅的 .ics 文件的 URL；或导入来自“邮件”的 .ics 文件。
+
+## 感谢
+
+[@junyilou](https://github.com/junyilou)
+
---
+++ timetable.py
@@ -0,0 +1,102 @@
+import datetime
+from datetime import datetime, timedelta
+
+def oeWeek(startWeek, endWeek, mode):
+    allWeek = range(startWeek, endWeek + 1)
+    oddWeek = []; evenWeek = []
+    for w in allWeek:
+        if w % 2 == 0: evenWeek.append(w)
+        else: oddWeek.append(w)
+    if mode: return oddWeek
+    else: return evenWeek
+
+
+def rgWeek(startWeek, endWeek): return list(range(startWeek, endWeek + 1))
+
+
+def timetable(maxWeek, classTime, starterDay, classes, oneClassTime=45):
+    maxWeek += 1
+
+    weeks = [None]
+    for i in range(1, maxWeek):
+        singleWeek = [None]
+        for d in range(0, 7):
+            singleWeek.append(starterDay)
+            starterDay = starterDay + timedelta(days = 1)
+        weeks.append(singleWeek)
+
+    iCalHeader = """BEGIN:VCALENDAR
+        METHOD:PUBLISH
+        VERSION:2.0
+        X-WR-CALNAME:课表
+        PRODID:-//Apple Inc.//Mac OS X 10.15.6//EN
+        X-WR-TIMEZONE:Asia/Shanghai
+        CALSCALE:GREGORIAN
+        BEGIN:VTIMEZONE
+        TZID:Asia/Shanghai
+        END:VTIMEZONE"""
+
+    createNow = datetime.now() - timedelta(hours = 8)
+    
+    allvEvent = ''
+    for Class in classes:
+        [Name, Teacher, Location, classWeek, classWeekday, classOrder] = Class[:]
+        Title = Name + "@" + Location
+        for timeWeek in classWeek:
+            classDate = weeks[timeWeek][classWeekday]
+            startTime = classTime[classOrder[0]]; endTime = classTime[classOrder[-1]]
+            classStartTime = classDate + timedelta(minutes = startTime[0] * 60 + startTime[1])
+            classEndTime = classDate + timedelta(minutes = endTime[0] * 60 + endTime[1] + oneClassTime)
+            Description = " 任课教师: " + Teacher + "。"
+            vEvent = "\nBEGIN:VEVENT"
+            vEvent += "\nDTEND;TZID=Asia/Shanghai:" + classEndTime.strftime('%Y%m%dT%H%M%S')
+            vEvent += "\nSUMMARY:" + Title
+            vEvent += "\nDTSTART;TZID=Asia/Shanghai:" + classStartTime.strftime('%Y%m%dT%H%M%S')
+            vEvent += "\nDESCRIPTION:" + Description
+            vEvent += "\nEND:VEVENT"
+            allvEvent += vEvent
+    allvEvent += "\nEND:VCALENDAR"
+
+    return iCalHeader + allvEvent
+
+
+def main():
+    maxWeek = 20
+    classTime = [None, (8, 20), (9, 15), (10, 20), (11, 15), (14, 00), (14, 55), 
+    (16, 00), (16, 55), (19, 0), (19, 55), (20, 50)]
+    starterDay = datetime(2020, 8, 31)
+    classes = [
+        ['数据科学1班', '张兴兰/陆艳军', '5-508', rgWeek(12, 19), 1, [3, 4]],
+        ['综合英语10班', '彭静', '1-603', rgWeek(4, 10), 2, [3, 4]],
+        ['综合英语10班', '彭静', '1-603', rgWeek(12, 16), 2, [3, 4]],
+        ['数据科学1班', '张兴兰/陆艳军', '5-508', rgWeek(12, 19), 2, [5, 6]],
+        ['算法设计与分析1班', '陈媛', '5-508', rgWeek(4, 10), 2, [7, 8]],
+        ['算法设计与分析1班', '陈媛', '5-508', rgWeek(12, 16), 2, [7, 8]],
+        ['综合英语10班', '彭静', '1-603', rgWeek(4, 10), 3, [3, 4]],
+        ['综合英语10班', '彭静', '1-603', rgWeek(12, 16), 3, [3, 4]],
+        ['算法设计与分析1班', '陈媛', '5-508', rgWeek(4, 10), 3, [7, 8]],
+        ['算法设计与分析1班', '陈媛', '5-508', rgWeek(12, 16), 3, [7, 8]],
+        ['数据科学1班', '张兴兰/陆艳军', '4-310', rgWeek(12, 19), 4, [1, 2]],
+        ['计算智能及应用1班', '石美凤', '5-508', rgWeek(6, 10), 4, [1, 2]],
+        ['高等工程数学1班', '许安见/谢挺', '5-509', rgWeek(7, 10), 4, [3, 4]],
+        ['高等工程数学1班', '许安见/谢挺', '5-509', rgWeek(12, 19), 4, [3, 4]],
+        ['英语听说9班', 'Chris', '1-608', rgWeek(4, 9), 4, [5, 6]],
+        ['学术规范与科技论文写作', '刘小洋', '5-508', rgWeek(6, 9), 4, [7, 8]],
+        ['学术规范与科技论文写作', '刘小洋', '5-508', rgWeek(6, 9), 4, [9, 10]],
+        ['计算智能及应用1班', '石美凤', '5-508', rgWeek(6, 10), 5, [1, 2]],
+        ['计算智能及应用1班', '石美凤', '5-508', rgWeek(12, 17), 5, [1, 2]],
+        ['高等工程数学1班', '许安见/谢挺', '5-508', rgWeek(7, 10), 5, [3, 4]],
+        ['高等工程数学1班', '许安见/谢挺', '5-508', rgWeek(12, 19), 5, [3, 4]],
+        ['机器学习及应用1班', '冯欣/龙建武', '5-508', rgWeek(6, 10), 5, [5, 6]],
+        ['机器学习及应用1班', '冯欣/龙建武', '5-508', rgWeek(12, 19), 5, [5, 6]],
+        ['中国特色社会主义理论与实践研究8班', '黎野', '1-605', rgWeek(4, 10), 5, [7, 8]],
+        ['中国特色社会主义理论与实践研究8班', '黎野', '1-605', rgWeek(12, 16), 5, [7, 8]],
+        ['机器学习及应用1班', '冯欣/龙建武', '5-508', rgWeek(6, 8), 5, [9, 10]],
+    ]
+    filename = 'timetable.ics'
+    with open(filename, 'w') as f:
+        f.write(timetable(maxWeek, classTime, starterDay, classes))
+
+
+if __name__ == '__main__':
+    main()
